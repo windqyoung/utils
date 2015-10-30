fd63ae14afd76a68f51bf83404058ca01ca1ccd9
windsor <txcses@gmail.com>
2015-10-30 09:09:49 +0800

    排序

---
+++ src/Artisan/OrderBy.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace Windqyoung\Utils\Artisan;
+
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Expression;
+
+
+class OrderBy
+{
+    const ASC = 1;
+    const DESC = 2;
+
+    private $value;
+
+    /**
+     * @var Expression|string
+     */
+    private $column;
+
+    public function __construct($value, $column, $defIsAsc = true)
+    {
+        $this->column = $column;
+
+        $this->setValue($value, $defIsAsc);
+    }
+
+    public function setValue($v, $defIsAsc)
+    {
+        if (in_array($v, [self::ASC, self::DESC]))
+        {
+            $this->value = $v;
+        } else
+        {
+            $this->value = $defIsAsc ? self::ASC : self::DESC;
+        }
+    }
+
+    /**
+     * @param Builder $query
+     */
+    public function apply($query)
+    {
+        $query->getQuery()->orderBy(
+            $this->getOrderColumn($query),
+            self::ASC == $this->value ? 'asc' : 'desc'
+        );
+    }
+
+    /**
+     * @param Builder $query
+     */
+    private function getOrderColumn($query)
+    {
+        if ($this->column instanceof Expression)
+        {
+            return $this->column;
+        }
+
+        return $query->getModel()->getTable() . '.' . $this->column;
+    }
+
+    /**
+     * 当前排序
+     */
+    public function value()
+    {
+        return $this->value;
+    }
+
+    /**
+     * 相反的排序
+     */
+    public function other()
+    {
+        return self::ASC == $this->value ? self::DESC : self::ASC;
+    }
+}
